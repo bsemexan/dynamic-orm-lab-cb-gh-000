3f11c8e3f3ce5d7e50bf5ac9ab5da9bd4ceb5e98
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 23:53:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,15 @@ class Student < InteractiveRecord
   end
 
   def self.column_names
-    self.each do |col_name|
-      attr_accessor col_name.to_sym
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
     end
+    column_names.compact
   end
 end
