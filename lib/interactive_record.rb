bac93aad1e6de5c05795055c32469a8c13d4b1c7
Berry  Semexan <bsemexan@gmail.com>
2018-02-28 00:01:30 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,10 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
 end
