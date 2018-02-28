c32f14af25b96d25e21897ef65a32a035d8dbf9b
Berry  Semexan <bsemexan@gmail.com>
2018-02-28 00:45:02 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
   def self.find_by(attribute)
     col_name = attribute.keys.first.to_s
     col_value = attribute.values.first
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{col_name}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE '#{col_name}'"
     DB[:conn].execute(sql, col_value)
   end
 end
